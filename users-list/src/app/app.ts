677831b98f0a40707332170c803077894edb9aab
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-02 06:13:22 +0200

    feat: add initial setup for UsersList application with Angular, including configuration files, components, and styles

---
+++ users-list/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Users } from './users/users';
+
+@Component({
+  selector: 'app-root',
+  imports: [ Users],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+}
---
+++ users-list/src/app/users/users.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-users',
+  imports: [NgClass],
+  templateUrl: './users.html',
+  styleUrl: './users.css',
+})
+export class Users {
+  users = [
+    {
+      id: 1,
+      username: 'Ahmed Ali',
+      email: 'ahmed@example.com',
+      phone: '01012345678',
+      birthdate: '1995-03-15',
+      role: 'admin',
+    },
+    {
+      id: 2,
+      username: 'Sara Mohamed',
+      email: 'sara@example.com',
+      phone: '01198765432',
+      birthdate: '1998-07-22',
+      role: 'user',
+    },
+    {
+      id: 3,
+      username: 'Omar Hassan',
+      email: 'omar@example.com',
+      phone: '01234567890',
+      birthdate: '1992-11-08',
+      role: 'moderator',
+    },
+    {
+      id: 4,
+      username: 'Nour Ibrahim',
+      email: 'nour@example.com',
+      phone: '01567890123',
+      birthdate: '2000-01-30',
+      role: 'user',
+    }
+  ];
+}
