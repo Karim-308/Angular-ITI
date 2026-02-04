814893d96be793f61a01e8eaeec048b6526570fd
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-04 09:59:34 +0200

    feat: Implement registration form with validation and submission handling

--- kika-store/src/app/register/register.ts
+++ kika-store/src/app/register/register.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required, Validators.pattern('^\S*$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8) , Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8) , Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
+  });
 
+  submitted = false;
+
+  submitRegister() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      console.log('Registration submitted:', this.registerForm.value);
+    }
+  }
 }
