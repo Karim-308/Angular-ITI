2d6c2a64ea51ef4b764855dfcb53952395d0c1c3
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-04 10:18:56 +0200

    feat: Enhance registration form validation and implement password mismatch check

--- kika-store/src/app/register/register.ts
+++ kika-store/src/app/register/register.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { FormGroup, FormControl, Validators ,AbstractControl} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,18 +12,26 @@ import { RouterLink } from '@angular/router';
 export class Register {
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
-    username: new FormControl('', [Validators.required, Validators.pattern('^\S*$')]),
+    username: new FormControl('', [Validators.required, Validators.pattern('^\\S*$')]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8) , Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8) , Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
-  });
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]),
+  }, { validators: this.passwordMatchValidator });
 
   submitted = false;
+
+    private router = inject(Router);
 
   submitRegister() {
     this.submitted = true;
     if (this.registerForm.valid) {
       console.log('Registration submitted:', this.registerForm.value);
+      this.router.navigate(['/'])
     }
+  }
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
   }
 }
