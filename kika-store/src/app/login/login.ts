6953e134320481c4d892dc096475331ca7f69b2f
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-04 09:18:25 +0200

    feat: Implement login form with validation and submission handling

--- kika-store/src/app/login/login.ts
+++ kika-store/src/app/login/login.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  submitted = false;
+
+  submitLogin(formData: { email: string; password: string }) {
+    this.submitted = true;
+    console.log('Login submitted:', formData);
+  }
+  
 }
